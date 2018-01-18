37c3b638a6444c80704702245d2644012f635cbf
yejihuang <yejihuang209592@sogou-inc.com>
2018-01-18 14:23:24 +0800

    翻转字符串的3种方法

--- com/yesco/learning/reserve.py
+++ com/yesco/learning/reserve.py
@@ -25,3 +25,34 @@ str2l = [int(x) for x in str[::-1]]
 print 'str翻转之前:', (str2l)
 reverse(str2l)
 print 'str翻转之后:', (str2l)
+
+reverse_string = "reverse"
+
+# 字符串反转的3种方法
+# 1.切片法（最简洁的一种）
+def reverse1(s):
+    return s[::-1]
+
+# 2.递归
+def reverse2(s):
+    if s=="":
+        return s
+    else:
+        return reverse2(s[1:]) + s[0]
+
+# 3.借用列表
+# Python中自带reverse()函数，可以处理列表的反转
+# join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串
+# # 如下例子
+# str = '-'
+# sequence = ('a', 'b', 'c')
+# str.join(sequence)
+# # 输出结果是：a-b-c
+def reverse3(s):
+    l = list(s)
+    l.reverse()
+    return "".join(l)
+
+print '1.切片法:', reverse1(reverse_string)
+print '2.递归:', reverse2(reverse_string)
+print '3.借用列表:', reverse3(reverse_string)
